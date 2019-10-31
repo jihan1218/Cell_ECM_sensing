35025a9d324ce1e56c3ec9c8069bbd2ccd8c7b5d
jihan1218 <jihan1218@gmail.com>
2019-10-30 19:38:39 -0700

    linux station

--- maxzproject.m
+++ maxzproject.m
@@ -1,22 +1,24 @@
 % max z projection
 clear all
 
-ch = 1;
-sample = 2;
-time = 2;
+ch = 0;
+%sample = 1;
+time = 4;
 
 
 %foldname = ['/Volumes/JIhan_SSD/Cellmechanics/on site contact guidance/Motility/'...
 %    '10x',filesep,sprintf('s%02d',sample)];
-foldname = ['/media/kimji/JIhan_SSD/Cellmechanics/on site contact guidance/Motility/'...
-    '20x',filesep,sprintf('s%02d',sample)];
+%foldname = ['/media/kimji/JIhan_SSD/Cellmechanics/on site contact guidance/Motility/'...
+%    '20x',filesep,sprintf('s%02d',sample)];
+foldname = ['/home/kimji/Project/Cell_mechanics/Collagen_geometry/'...
+    'cell shape dynamics/circle 21C'];
 
-newfold = [foldname,filesep,'xyt_max_bright'];
+newfold = [foldname,filesep,'xyt_max'];
 mkdir(newfold);
 count = 0;
 
 for t = 1: time
-    imfold = [foldname,filesep,sprintf('xyzt_%02d',t)];
+    imfold = [foldname,filesep,sprintf('xyzt_%02d_15mpf',t)];
     d = dir(imfold);
     reg = struct2cell(d);
     ind = find(contains(reg(1,:),'z00_ch00.tif'));
@@ -25,7 +27,7 @@ for t = 1: time
         if lt >= 100
             img = loadimgs([imfold,filesep,sprintf('*_t%03d_*_ch%02d.tif',ti,ch)]);
         elseif lt >= 10 && lt <100
-            img = loadimgs([imfold,filesep,sprintf('*_t%2d_*_ch%02d.tif',ti,ch)]);
+            img = loadimgs([imfold,filesep,sprintf('*_t%02d_*_ch%02d.tif',ti,ch)]);
         elseif lt < 10 
             img = loadimgs([imfold,filesep,sprintf('*_t%d_*ch%02d.tif',ti,ch)]);
         end
@@ -33,7 +35,7 @@ for t = 1: time
             mip = max(img,[],3);
       
             count = count + 1;
-            imwrite(mip,[newfold,filesep,sprintf('talin_t%02d.tif',count)]);
+            imwrite(mip,[newfold,filesep,sprintf('max_t%02d.tif',count)]);
         
         
     end
